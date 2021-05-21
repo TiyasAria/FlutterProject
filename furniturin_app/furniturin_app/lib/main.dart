687f71eca80f25bb822d73d13ede8e5dac524168
Tiyas Aria Pratiwi <71452274+TiyasAria@users.noreply.github.com>
2021-05-21 20:47:38 +0700

    new project

---
+++ furniturin_app/furniturin_app/lib/main.dart
@@ -0,0 +1,252 @@
+import 'package:flutter/material.dart';
+
+import 'bloc/furnitur_store_bloc.dart';
+import 'provider/furnitur_provider.dart';
+import 'screens/furnitur_store_cart.dart';
+import 'screens/furnitur_store_list.dart';
+import 'utils/colors.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+const cartBarHeight = 100.0;
+const _panelTransition = Duration(milliseconds: 750);
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Furniturin Store',
+      color: Colors.red,
+      debugShowCheckedModeBanner: false,
+      home: GroceryStoreHome(),
+    );
+  }
+}
+
+class GroceryStoreHome extends StatefulWidget {
+  @override
+  _GroceryStoreHomeState createState() => _GroceryStoreHomeState();
+}
+
+class _GroceryStoreHomeState extends State<GroceryStoreHome> {
+  final bloc = FurniturStoreBloc();
+
+  void _onVerticalGesture(DragUpdateDetails details) {
+    if (details.primaryDelta < -7) {
+      bloc.changeToCart();
+    } else if (details.primaryDelta > 12) {
+      bloc.changeToNormal();
+    }
+  }
+
+  double _getTopForWhitePanel(FurniturState state, Size size) {
+    if (state == FurniturState.normal) {
+      return -cartBarHeight + kToolbarHeight;
+    } else if (state == FurniturState.cart) {
+      return -(size.height - kToolbarHeight - cartBarHeight / 2);
+    }
+    return 0.0;
+  }
+
+  double _getTopForBlackPanel(FurniturState state, Size size) {
+    if (state == FurniturState.normal) {
+      return size.height - cartBarHeight;
+    } else if (state == FurniturState.cart) {
+      return cartBarHeight / 2;
+    }
+    return 0.0;
+  }
+
+  double _getTopForAppBar(FurniturState state) {
+    if (state == FurniturState.normal) {
+      return 0.0;
+    } else if (state == FurniturState.cart) {
+      return -cartBarHeight;
+    }
+    return 0.0;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+
+    return FurniturProvider(
+      bloc: bloc,
+      child: AnimatedBuilder(
+        animation: bloc,
+        builder: (context, __) {
+          return Scaffold(
+            backgroundColor: Colors.black,
+            body: Stack(
+              children: [
+                AnimatedPositioned(
+                  curve: Curves.decelerate,
+                  duration: _panelTransition,
+                  left: 0,
+                  right: 0,
+                  top: _getTopForWhitePanel(bloc.furniturState, size),
+                  height: size.height - kToolbarHeight,
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.only(
+                      bottomLeft: Radius.circular(30),
+                      bottomRight: Radius.circular(30),
+                    ),
+                    child: Container(
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                      ),
+                      child: FurniturStoreList(),
+                    ),
+                  ),
+                ),
+                AnimatedPositioned(
+                  curve: Curves.decelerate,
+                  duration: _panelTransition,
+                  left: 0,
+                  right: 0,
+                  top: _getTopForBlackPanel(bloc.furniturState, size),
+                  height: size.height - kToolbarHeight,
+                  child: GestureDetector(
+                    onVerticalDragUpdate: _onVerticalGesture,
+                    child: Container(
+                      color: Colors.black,
+                      child: Column(
+                        children: [
+                          Padding(
+                            padding: const EdgeInsets.all(25.0),
+                            child: AnimatedSwitcher(
+                              duration: _panelTransition,
+                              child: SizedBox(
+                                height: kToolbarHeight,
+                                child: bloc.furniturState == FurniturState.normal
+                                    ? Row(
+                                  children: [
+                                    Text(
+                                      'Cart',
+                                      style: TextStyle(
+                                        color: Colors.white,
+                                        fontWeight: FontWeight.bold,
+                                        fontSize: 20,
+                                      ),
+                                    ),
+                                    Expanded(
+                                      child: SingleChildScrollView(
+                                        scrollDirection: Axis.horizontal,
+                                        child: Padding(
+                                          padding: const EdgeInsets.symmetric(horizontal: 5.0),
+                                          child: Row(
+                                            children: List.generate(
+                                              bloc.cart.length,
+                                                  (index) => Padding(
+                                                padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                                                child: Stack(
+                                                  children: [
+                                                    Hero(
+                                                      tag: 'list_${bloc.cart[index].product.name}details',
+                                                      child: CircleAvatar(
+                                                        backgroundColor:
+                                                        Colors.white,
+                                                        backgroundImage:
+                                                        AssetImage(
+                                                          bloc
+                                                              .cart[index]
+                                                              .product
+                                                              .image,
+                                                        ),
+                                                      ),
+                                                    ),
+                                                    Positioned(
+                                                      right: 0,
+                                                      child: CircleAvatar(
+                                                        radius: 10,
+                                                        backgroundColor:
+                                                        Colors.red,
+                                                        child: Text(
+                                                          bloc.cart[index]
+                                                              .quantity
+                                                              .toString(),
+                                                          style: TextStyle(
+                                                            color: Colors
+                                                                .white,
+                                                          ),
+                                                        ),
+                                                      ),
+                                                    ),
+                                                  ],
+                                                ),
+                                              ),
+                                            ),
+                                          ),
+                                        ),
+                                      ),
+                                    ),
+                                    CircleAvatar(
+                                      backgroundColor: Color(0xFFF4C459),
+                                      child: Text(
+                                        bloc.totalCartElements().toString(),
+                                      ),
+                                    ),
+                                  ],
+                                )
+                                    : SizedBox.shrink(),
+                              ),
+                            ),
+                          ),
+                          Expanded(
+                            child: FurniturStoreCart(),
+                          ),
+
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+                AnimatedPositioned(
+                  curve: Curves.decelerate,
+                  duration: _panelTransition,
+                  top: _getTopForAppBar(bloc.furniturState),
+                  left: 0,
+                  right: 0,
+                  child: _AppBarGrocery(),
+                ),
+              ],
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
+
+class _AppBarGrocery extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(top:30.0),
+      child: Container(
+        padding: const EdgeInsets.only(top: 15.0),
+        height: kToolbarHeight,
+        color: backgroundColor,
+        child: Row(
+          children: [
+            BackButton(
+              color: Colors.black,
+            ),
+            const SizedBox(
+              width: 10.0,
+            ),
+            Expanded(
+              child: Text(
+                "Furniturin",
+                style: TextStyle(color: Colors.black),
+              ),
+            ),
+            IconButton(icon: Icon(Icons.settings), onPressed: () => null)
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ furniturin_app/furniturin_app/lib/screens/furnitur_store_list.dart
@@ -0,0 +1,103 @@
+
+
+import 'package:flutter/material.dart';
+import 'package:furniturin_app/main.dart';
+import 'package:furniturin_app/provider/furnitur_provider.dart';
+import 'package:furniturin_app/utils/colors.dart';
+import 'package:furniturin_app/widgets/stragged_dual_view.dart';
+
+import 'furnitur_store_details.dart';
+
+class FurniturStoreList extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final bloc = FurniturProvider.of(context).bloc;
+    return Container(
+      color: backgroundColor,
+      padding: const EdgeInsets.only(top: cartBarHeight, left: 10, right: 10),
+      child: StaggeredDualView(
+        aspectRatio: 0.82,
+        itemPercent: 0.25,
+        spacing: 20,
+        itemBuilder: (context, index) {
+          final product = bloc.catalog[index];
+          return GestureDetector(
+            onTap: () {
+              Navigator.of(context).push(
+                PageRouteBuilder(
+                  transitionDuration: const Duration(milliseconds: 650),
+                  pageBuilder: (context, animation, __) {
+                    return FadeTransition(
+                      opacity: animation,
+                      child: FurniturStoreDetails(
+                          product: product,
+                          onProductAdded: (){
+                            bloc.addProduct(product);
+                          }
+                      ),
+                    );
+                  },
+                ),
+              );
+            },
+            child: Card(
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(20.0),
+              ),
+              elevation: 8,
+              shadowColor: Colors.black45,
+              color: Colors.white,
+              child: Padding(
+                padding: const EdgeInsets.all(15.0),
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Expanded(
+                      child: Hero(
+                        tag: 'list_${product.name}',
+                        child: Image.asset(
+                          product.image,
+                          fit: BoxFit.contain,
+                        ),
+                      ),
+                    ),
+                    SizedBox(
+                      height: 5.0,
+                    ),
+                    Text(
+                      '\Rp' + product.price.toString(),
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        color: Colors.black,
+                        fontSize: 14,
+                      ),
+                    ),
+                    SizedBox(
+                      height: 15.0,
+                    ),
+                    Text(
+                      product.name,
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 12,
+                      ),
+                    ),
+                    Text(
+                      product.left,
+                      style: TextStyle(
+                        color: Colors.grey,
+                        fontSize: 10,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          );
+        },
+        itemCount: bloc.catalog.length,
+      ),
+    );
+  }
+}
